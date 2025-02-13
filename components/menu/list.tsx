f18109e63161f7274e51091aa4bb35ad8440a0ec
pooyam99 <pooyam1999@gmail.com>
2025-02-14 02:25:53 +0330

    feat: create `MenuCard` and `MenuList` components

---
+++ components/menu/list.tsx
@@ -0,0 +1,24 @@
+import { FlashList } from '@shopify/flash-list'
+import React from 'react'
+import { FlatList, View } from 'react-native'
+
+import MenuCard from './card'
+import { MenuItemData } from './dummyData'
+
+const MenuList = () => {
+  return (
+    <View>
+      <FlatList
+        data={MenuItemData}
+        renderItem={({ item }) => (
+          <View style={{ paddingVertical: 3, paddingHorizontal: 4 }}>
+            <MenuCard {...item} />
+          </View>
+        )}
+        // estimatedItemSize={50}
+      />
+    </View>
+  )
+}
+
+export default MenuList
