e1d84208858ac0bf758de436108089795692d4f1
pooyam99 <pooyam1999@gmail.com>
2025-02-14 02:54:54 +0330

    feat: install and setup `NativeWind` and `TailwindCSS`

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}", // Adjust to match your app directory
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
