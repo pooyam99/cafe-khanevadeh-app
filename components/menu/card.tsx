3c8916d605ca6349795504d1fb59f7b76a2e71a4
pooyam99 <pooyam1999@gmail.com>
2025-02-14 22:55:28 +0330

    refactor: switch to Tailwind styling

--- components/menu/card.tsx
+++ components/menu/card.tsx
@@ -5,41 +5,12 @@ import { MenuItemT } from "@/lib/types/menu";
 
 const MenuCard = (item: MenuItemT) => {
   return (
-    <View
-      style={{
-        direction: "rtl",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        width: `${100}%`,
-        height: 70,
-        paddingHorizontal: 12,
-        backgroundColor: "#0f0d0a",
-        borderRadius: 15,
-      }}>
-      <View
-        style={{
-          width: "auto",
-          height: `${100}%`,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: 4,
-        }}>
-        <Text style={{ color: "white", fontSize: 24 }}>{item.title}</Text>
-        <Text style={{ color: "white", fontSize: 16 }}>{item.description}</Text>
+    <View className="flex h-20 w-full flex-row items-center justify-between rounded-2xl bg-[#0f0d0a] px-3">
+      <View className="flex h-full w-auto flex-col items-start justify-center gap-1">
+        <Text className="text-lg text-white">{item.title}</Text>
+        <Text className="text-base text-white">{item.description}</Text>
       </View>
-      <Text
-        style={{
-          color: "white",
-          borderColor: "#dfa839",
-          borderWidth: 2,
-          paddingHorizontal: 8,
-          paddingVertical: 4,
-          borderRadius: 25,
-        }}>
+      <Text className="rounded-full border-2 border-[#dfa839] px-2 py-1 text-white">
         {item.price}
       </Text>
     </View>
