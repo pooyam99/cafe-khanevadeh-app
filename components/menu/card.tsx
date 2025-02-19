ecc6021a6a1b3172dc46a7dd01a46ba54babe797
pooyam99 <pooyam1999@gmail.com>
2025-02-20 03:28:24 +0330

    refactor: update `MenuCard` styling

--- components/menu/card.tsx
+++ components/menu/card.tsx
@@ -2,17 +2,24 @@ import React from "react";
 import { Text, View } from "react-native";
 
 import { MenuItemT } from "@/lib/types/menu";
+import { Dialog } from "react-native-paper";
 
 const MenuCard = (item: MenuItemT) => {
   return (
-    <View className="flex h-20 w-full flex-row items-center justify-between rounded-2xl bg-[#0f0d0a] px-3">
+    <View className="flex h-20 w-full flex-row items-center justify-between rounded-2xl bg-[#0f0d0a] px-4">
       <View className="flex h-full w-auto flex-col items-start justify-center gap-1">
-        <Text className="text-lg text-white">{item.title}</Text>
-        <Text className="text-base text-white">{item.description}</Text>
+        <Text className="font-vazirmatn-bold text-lg text-white">
+          {item.attributes.title}
+        </Text>
+        <Text className="font-vazirmatn-light text-base text-white">
+          {item.attributes.description}
+        </Text>
       </View>
-      <Text className="rounded-full border-2 border-[#dfa839] px-2 py-1 text-white">
-        {item.price}
-      </Text>
+      <View className="flex items-center justify-center rounded-full border-2 border-[#dfa839] px-3 py-1">
+        <Text className="font-vazirmatn text-white">
+          {item.attributes.price}
+        </Text>
+      </View>
     </View>
   );
 };
